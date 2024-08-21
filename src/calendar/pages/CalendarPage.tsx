a6b822965369772280e4d1f34279184e9b4bc269
feliegonzalezmv <53414652+feliegonzalezmv@users.noreply.github.com>
2024-08-21 15:54:03 -0500

    Add react big calendar

--- src/calendar/pages/CalendarPage.tsx
+++ src/calendar/pages/CalendarPage.tsx
@@ -1,9 +1,43 @@
+import { Calendar, CalendarProps } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+
 import { NavBar } from "../components/NavBar";
+import { localizer } from "../../helpers";
+
+const events = [
+  {
+    title: "Boss birthday",
+    notes: "Buy cupcake",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: { _id: "123", name: "Felipe" },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event: CalendarProps) => {
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+    console.log("event :>> ", event);
+    return { style };
+  };
   return (
     <>
       <NavBar></NavBar>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   );
 };
